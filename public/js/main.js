cb46cebcfe44f31d447a1113cb2605d153ecd9b5
Matthew Gerring <gerring.matthew@gmail.com>
2013-02-02 23:18:01 -0800

    locate me

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,7 @@
 var map = L.map('map',{maxZoom: 19});
 var layer = new L.StamenTileLayer("terrain");
-map.addLayer(layer).setView(new L.LatLng(37.7810841,-122.4105332), 13);
+map.addLayer(layer).locate({setView: true, maxZoom: 16});
+
 
 /*(function($){
 
