c85305ea54acf4bc2a0cf6c33b16be4fbf87ca7f
Matthew Gerring <gerring.matthew@gmail.com>
2013-02-03 11:01:20 -0800

    fixed in firefox

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,9 @@
-var map = L.map('map',{maxZoom: 19});
-var layer = new L.StamenTileLayer("toner-lite");
-map.addLayer(layer).locate({setView: true, maxZoom: 16});
-
+var map,
+	layer;
 (function($){
+	map = L.map('map',{maxZoom: 19});
+	layer = new L.StamenTileLayer("toner-lite");
+	map.addLayer(layer).locate({setView: true, maxZoom: 16});
 	$('#splash button').click(function(e){
 		e.preventDefault();
 		$('#splash').hide();
