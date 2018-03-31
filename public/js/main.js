d07dee3497896f4bcd617ade6f77d4efa06329e6
Matthew Gerring <gerring.matthew@gmail.com>
2018-03-31 02:10:27 -0700

    Fixed styling, added notes, finally set a cookie on the splash so it doesn't come back every time you load the site, fixed the drawer and added useful things to it

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,9 @@
 var map,
 	layer;
 (function($){
+  if (getCookie('datafest-moneymap') !== 'hide-overlay-on-load') {
+	  $('#splash').show();
+  }
   layer = new L.StamenTileLayer("toner-lite");
 	map = L.map('mapLayer', {
 	  center: L.latLng(37.476526, -119.938271),
@@ -11,7 +14,11 @@ var map,
 	$('#splash button').click(function(e){
 		e.preventDefault();
 		$('#splash').hide();
+		setCookie('datafest-moneymap','hide-overlay-on-load',30);
 	});
+	$('#about').click(function(e) {
+	  $('#splash').show();
+  });
 	$('footer .drawer').toggle(
 		function(){
 			$('body').addClass('footer-up');
@@ -21,3 +28,26 @@ var map,
 		}
 	);
 })($);
+
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+function eraseCookie(name) {
+    document.cookie = name+'=; Max-Age=-99999999;';
+}
