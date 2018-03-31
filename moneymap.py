ddeba336327d9cf4c6cf2c75d94f7eadbd4d480b
Matthew Gerring <gerring.matthew@gmail.com>
2018-03-31 00:48:24 -0700

    Fix CSS

--- moneymap.py
+++ moneymap.py
@@ -5,7 +5,7 @@ ROOT = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__, static_folder=os.path.join(ROOT, 'public'), static_url_path='/public')
 app.config.from_object('config')
-app.debug = True
+#app.debug = True
 
 @app.route("/")
 def home():
--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,13 @@
 var map,
 	layer;
 (function($){
-	map = L.map('map',{maxZoom: 19});
-	layer = new L.StamenTileLayer("toner-lite");
-	map.addLayer(layer).setView(L.latLng(37.166111,-119.449444), 6);
+  layer = new L.StamenTileLayer("toner-lite");
+	map = L.map('mapLayer', {
+	  center: L.latLng(37.476526, -119.938271),
+    zoom: 6,
+	  maxZoom: 19
+	});
+	map.addLayer(layer);
 	$('#splash button').click(function(e){
 		e.preventDefault();
 		$('#splash').hide();
